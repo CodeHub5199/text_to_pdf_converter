ce7422c97f96388aef8479f00579ecb348173ec4
Atharv Bhasme <94709794+CodeHub5199@users.noreply.github.com>
2023-07-18 14:55:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from fpdf import FPDF
+import os
 
 def convert_to_pdf(text):
     pdf = FPDF()
@@ -7,6 +8,12 @@ def convert_to_pdf(text):
     pdf.set_font("Arial", size=12)
     pdf.cell(0, 10, txt=text)
     return pdf
+
+def save_uploaded_file(file):
+    file_path = os.path.join("uploads", file.name)
+    with open(file_path, "wb") as f:
+        f.write(file.getbuffer())
+    return file_path
 
 def main():
     st.title("Text to PDF Converter")
@@ -16,7 +23,9 @@ def main():
     file = st.file_uploader("Choose a text file", type=["txt"])
     
     if file is not None:
-        file_contents = file.read().decode("utf-8")
+        file_path = save_uploaded_file(file)
+        file_contents = open(file_path, "r").read()
+        st.write(file_path)
         
         # Convert text to PDF
         pdf = convert_to_pdf(file_contents)
